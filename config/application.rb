8c30c1254b2936252d960037cfbfb03de664d399
Yuki Sato <yuki.sato@livesense.co.jp>
2025-04-30 14:51:37 +0900

    訳文の記述箇所を変更。不要な記述を削除

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.load_path += Dir[Rails.root.join("config", "locale", "*.{rb,yml}")]
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.

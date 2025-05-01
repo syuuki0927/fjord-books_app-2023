ac71ca070d953325fb644186e53d8cb344bb57b5
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-01 16:59:58 +0900

    主要機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show]
   def index
     @users = User.order(:emal).page(params[:page])
-    # @users = User.all
   end
 
-  def show; end
-
-  def edit; end
-
-  # PATCH/PUT /books/1 or /books/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: t('controllers.common.notice_update', name: User.model_name.human) }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+  def show
+    @current_user = current_user
+    @self_user = (current_user == @user)
   end
 
   def set_user

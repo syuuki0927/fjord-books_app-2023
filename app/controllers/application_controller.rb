11b93d7bee980276fd3c1a3b439d301d707db22d
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-08 15:49:03 +0900

    サインイン等の後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_out_path_for(_)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end

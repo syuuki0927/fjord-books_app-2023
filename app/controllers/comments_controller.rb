752a853a4b49efa179c59d1f5fb8f0e84836c868
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-08 14:53:58 +0900

    i18nの適用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show; end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
+  before_action :set_comment, only: %i[edit update destroy]
 
   # GET /comments/1/edit
   def edit; end
@@ -25,7 +12,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.commentable, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -36,7 +23,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment.commentable, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human) }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -48,7 +35,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to @comment.commentable, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
     end
   end
 

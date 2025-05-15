12ed70fb04a97c70320992fb95e3d76d7563e219
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-15 13:31:39 +0900

    削除、編集を投稿したユーザーのみができるように変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,47 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[edit update destroy]
-
   # GET /comments/1/edit
-  def edit; end
+  def edit
+    @comment = Comment.find(params[:id])
+
+    redirect_to @comment.commentable if @comment.user != current_user
+  end
 
   # POST /comments or /comments.json
   def create
     @comment = Comment.new(comment_params.merge(user_id: current_user.id))
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    @comment = Comment.find(params[:id])
+    if @comment.user == current_user && @comment.update(comment_params)
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    @comment = Comment.find(params[:id])
     @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+    if @comment.user == current_user && @comment.destroy
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_unable_destroy', name: Comment.model_name.human)
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def comment_params
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[show edit update destroy]
-
   # GET /reports or /reports.json
   def index
     @reports = Report.all
   end
 
   # GET /reports/1 or /reports/1.json
-  def show; end
+  def show
+    @report = Report.find(params[:id])
+  end
 
   # GET /reports/new
   def new
@@ -17,47 +17,45 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit; end
+  def edit
+    @report = Report.find(params[:id])
+
+    redirect_to report_url(@report) if @report.user != current_user
+  end
 
   # POST /reports or /reports.json
   def create
     @report = Report.new(report_params.merge(user_id: current_user.id))
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    @report = Report.find(params[:id])
+    if @report.user == current_user && @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
-    @report.destroy
+    @report = Report.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+    if @report.user == current_user && @report.destroy
+      redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+    else
+      redirect_to report_url(@report), notice: t('controllers.common.notice_unable_destroy', name: Report.model_name.human)
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_report
-    @report = Report.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def report_params

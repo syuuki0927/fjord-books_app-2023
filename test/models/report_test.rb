4e131a11f865e89e4e5bc1f3cb15ebc43c80dbaf
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-16 10:17:52 +0900

    モデルテストの記述

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,12 +3,33 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  def correct_user_editable
-    report = Report.find_by(user: 'one')
-    user = User.find_by(name: 'one')
-    assert(report.editable?(user))
+  test 'correct_user_editable' do
+    report = reports(:hoge_report)
+    user = users(:alice)
+    assert report.editable?(user)
   end
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'incorrect_user_unable_edit' do
+    report = reports(:hoge_report)
+    user = users(:two)
+    assert_not report.editable?(user)
+  end
+
+  test 'created_on' do
+    assert_equal(Date.new(2025, 5, 16), reports(:hoge_report).created_on)
+  end
+
+  test 'send_mentions' do
+    report = reports(:hoge_report)
+    assert_equal(report.mentioning_reports, [])
+
+    report.content = 'http://localhost:3000/reports/1'
+
+    report.send(:save_mentions)
+    assert_equal(report.mentioning_reports, [Report.find(1)])
+
+    report.content = 'http://localhost:3000/reports/1\nhttp://localhost:3000/reports/1\nhttp://localhost:3000/reports/2'
+    report.send(:save_mentions)
+    assert_equal(report.mentioning_reports, [Report.find(1), Report.find(2)])
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test 'test_name_or_email' do
+    user = users(:alice)
+
+    assert_equal(user.name_or_email, 'alice')
+
+    user.name = nil
+
+    assert_equal(user.name_or_email, 'alice@example.com')
+  end
 end

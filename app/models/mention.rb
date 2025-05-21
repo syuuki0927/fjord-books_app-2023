eb52ddd4762b33c2c4e991f72a050858ea335e23
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-21 14:02:42 +0900

    mentionのvalidationを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :report_from, class_name: 'Report'
   belongs_to :report_to, class_name: 'Report'
+
+  validates :report_from, uniqueness: { scope: :report_to }
 end

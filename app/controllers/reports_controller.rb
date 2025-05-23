799d080a8ade2141c49dcdd8834b77d88309d4c6
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-23 10:17:30 +0900

    誤ったinverse_ofの関連付けを修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -39,8 +39,8 @@ class ReportsController < ApplicationController
   end
 
   def destroy
-    @report = current_user.reports.find(params[:id])
-    @report.destroy
+    report = current_user.reports.find(params[:id])
+    report.destroy
 
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning, class_name: 'Mention', foreign_key: 'report_from_id', dependent: :destroy, inverse_of: :report_to
+  has_many :mentioning, class_name: 'Mention', foreign_key: 'report_from_id', dependent: :destroy, inverse_of: :report_from
   has_many :mentioning_reports, through: :mentioning, source: :report_to
 
-  has_many :mentioned, class_name: 'Mention', foreign_key: 'report_to_id', dependent: :destroy, inverse_of: :report_from
+  has_many :mentioned, class_name: 'Mention', foreign_key: 'report_to_id', dependent: :destroy, inverse_of: :report_to
   has_many :mentioned_reports, through: :mentioned, source: :report_from
 
   validates :title, presence: true

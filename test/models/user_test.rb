011b6d3bc9fa04d4b4e3fe94519dc7fb6f1e6dd0
Yuki Sato <yuki.sato@livesense.co.jp>
2025-05-26 14:00:29 +0900

    userのモデルテストの可読性向上

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,11 +5,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'test_name_or_email' do
     user = users(:alice)
-
-    assert_equal(user.name_or_email, 'alice')
+    assert_equal('alice', user.name_or_email)
 
     user.name = nil
-
-    assert_equal(user.name_or_email, 'alice@example.com')
+    assert_equal('alice@example.com', user.name_or_email)
   end
 end
